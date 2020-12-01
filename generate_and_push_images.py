790cff9890742a1287cf462c24378c02e1600ac4
Mohammed Fellaji <mohammed.fellaji@supelec.fr>
2020-12-01 07:46:57 +0100

    fix deployment on minikue (logs folder was missing in the python image)

--- generate_and_push_images.py
+++ generate_and_push_images.py
@@ -7,15 +7,21 @@ TO BE RUN FROM root
   $$ python3 generate_push.py
 '''
 
+
 p   = ["dashboard", "estimator", "learner", "monitor", "predictor"]
-tag = 1
+tag_p = 3
+
+
+os.system(f"docker build -f docker/Dockerfile.python-main -t fellajimed/tweetoscope-python:main-{tag_p} .")
+os.system(f"docker push fellajimed/tweetoscope-python:main-{tag_p}")
+
 
 for d in p :
-    os.system(f"docker build -f docker/Dockerfile.{d} -t fellajimed/tweetoscope-python:{d}-{tag} .")
-    os.system(f"docker push fellajimed/tweetoscope-python:{d}-{tag}")
+    os.system(f"docker build -f docker/Dockerfile.{d} -t fellajimed/tweetoscope-python:{d}-{tag_p} .")
+    os.system(f"docker push fellajimed/tweetoscope-python:{d}-{tag_p}")
 
 c = ["generator", "collector"]
-
+tag_c = 1
 for d in c :
-    os.system(f"docker build -f docker/Dockerfile.{d} -t fellajimed/tweetoscope-cpp:{d}-{tag} .")
-    os.system(f"docker push fellajimed/tweetoscope-cpp:{d}-{tag}")
+    os.system(f"docker build -f docker/Dockerfile.{d} -t fellajimed/tweetoscope-cpp:{d}-{tag_c} .")
+    os.system(f"docker push fellajimed/tweetoscope-cpp:{d}-{tag_c}")
